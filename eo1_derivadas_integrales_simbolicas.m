22cbb20242758bc92b3eb69159a7a416f2b34b2b
Daniel Salas <danielsalasflores122@gmail.com>
2024-05-08 13:11:37 -0600

    Nuevo repositorio de calculo integral

---
+++ eo1_derivadas_integrales_simbolicas.m
@@ -0,0 +1,9 @@
+%%ITESS-TICS
+%%Enero-Junio 2024
+%%TI202 CALCULO INTEGRAL
+%%Daniel Salas Flores
+%%Francisco Javier Montecillo Puente
+%% Derivadas e integrales simbolicas.
+%%06 mayo 2024
+pkg load symbolic
+syms x
